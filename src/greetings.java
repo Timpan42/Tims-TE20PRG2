485fe317a66c685de9c119288e9b466219ee722d
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2022-08-25 15:49:49 +0200

    Koden funkar typ men sidan gillar inte mit svar

--- src/greetings.java
+++ src/greetings.java
@@ -2,11 +2,14 @@ import javax.swing.*;
 
 public class greetings {
     public static void main(String[] args){
-        String e = JOptionPane.showInputDialog("say hej");
-        String[] found = e.split("");
-        int p = found.length - 2;
-        int s = p*2;
-
+        String x = JOptionPane.showInputDialog("say hej");
+        String[] found = x.split("");
+        int y = found.length - 2;
+        int z = y*2;
+        String s = "";
+        for (int i = 0; i < z; i++){
+            s = s + "e";
+        }
         System.out.print("h" + s + "y");
     }
 }
