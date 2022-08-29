9e3c3ce040f6431d93e55ce9f835e38eb06be9b6
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2022-08-29 09:20:10 +0200

    Boten funkar, man kan svara och få svar och en fråga tillbaka, den ge också dem svar som personer gede till baka, dock kan den ge samma svar eller fråga tillbaka och det gåt inte att spara svaren i en fil.

--- src/prat_app.java
+++ src/prat_app.java
@@ -4,21 +4,50 @@ import java.util.Random;
 public class prat_app {
     public static void main (String[]args){
         Random rand = new Random();
-        String[] good ={"va bra","trevligt"};
-        String[] bad ={"du dålig","vem fan är du!"};
         String n = "\n";
-        String[] ansers = {""};
+        String[] bot ={
+                "Va bra",
+                "Trevligt",
+                "Du dålig",
+                "Vem fan är du!",
+                "Din moder",
+                "Jag försöker räkna ut vad du sa och jag kom fram till att du har fel",
+                "Jag tycker synd om dig",
+                "Du borde söka en bättre psykolog",
+                "Jag är ingen robot ;)",
+                ">:[",
+                ":|"
+        };
+        String[] questions = {
+                "Det känns lite onödigt?",
+                "Varför kom du hit?",
+                "Har du tänkt på att sova?",
+                "Är du en psykopat?",
+                "Har du tänkt att investera i drog industrin?",
+                "#) tankar?"
+        };
+        String pAnswer = "";
+        String bQuestion = "";
+        String lastQuestion = "";
         Scanner sc = new Scanner(System.in);
 
-
         System.out.print("Hej min vän, hur är det i dag?" + n + ":");
-        String str = sc.nextLine();
-        System.out.println(""+ bad[rand.nextInt(bad.length)]);
+        String str = n + sc.nextLine();
+        System.out.println(""+ bot[rand.nextInt(bot.length)]);
 
         for (int i=0; i<5; i++){
-            System.out.print("?" + n + ":");
-            ansers = sc.nextLine();
-            System.out.println(""+ bad[rand.nextInt(bad.length)]);
+            bQuestion = questions[rand.nextInt(questions.length)];
+            if (bQuestion == lastQuestion) {
+                bQuestion = questions[rand.nextInt(questions.length)];
+            }
+            lastQuestion = bQuestion;
+            System.out.print(bQuestion + n + ":");
+            pAnswer = pAnswer + n + sc.nextLine();
+            System.out.println(""+ bot[rand.nextInt(bot.length)]);
         }
+        System.out.print("Svaren från personen" + n + str + pAnswer);
+        System.out.print(n + "Några sista åsikter om boten?" + n +":");
+        sc.nextLine();
+        System.out.print("Tråkigt för dig då");
     }
 }
