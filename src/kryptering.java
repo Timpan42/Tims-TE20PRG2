36898c884e6503efe0b28a1d8a2e00ddef07fd6f
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2022-09-02 13:35:06 +0200

    Har ett problem när jag kör programet och jag förstår inte vad problemet är också problem med att komma på ett sätt att kryptera meningen man skriver in

--- src/kryptering.java
+++ src/kryptering.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
 
 public class kryptering {
+
     public static void main(String[] args) {
+        String n = "\n";
+        char me = 'h';
+        char ke = 'r';
+        char cr = (char)(me^ke);
+        System.out.println(cr);
+        System.out.println((char)(cr^ke));
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(n + "Skriv vad du vill" + n + ":");
+        String str = sc.nextLine();
+        char[] ch = new char[str.length()];
+        for (int i = 0; i <= str.length(); i++){
+            ch[i] = str.charAt(i);
+        }
+        System.out.println(ch[2]);
+    }
+    public static void krypt1(){
         int m = 123;
         int k = 567813;
         int c = m^k;
         System.out.println(Integer.toBinaryString(c));
         System.out.println(c^k);
-
-        char me = 'h';
-        char cr = (char)(me^k);
-        System.out.println(cr);
-        System.out.println((char)(cr^k));
     }
 }
