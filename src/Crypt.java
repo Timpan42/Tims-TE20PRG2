126143cee0a650fbed5211c101f894e6fcfbc1a6
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2022-09-16 13:37:49 +0200

    jag har en nykel men vet inte om den funkar

--- src/Crypt.java
+++ src/Crypt.java
@@ -1,29 +1,43 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Random;
 
 public class Crypt {
+    String key;
     String filnamn;
     String meddelande;
-    String key;
-    public void readTextFile(String filnamn){
+    Random r = new Random();
+
+    //ska öppna och läsa en text fil
+    public String readTextFile(String filnamn){
+        String mes = "";
         try {
             BufferedReader br = new BufferedReader(new FileReader(filnamn));
+            mes = br.readLine();
         } catch (IOException e){
             e.printStackTrace();
         }
+        return mes;
     }
 
+// det crypterande medelande sparas
     public void crypt(String key, String meddelande){
         if (key == null)
-            readKey();
+            key = readKey();
         encrypt(meddelande, key);
 
     }
-    public void  readKey(){
-
+    //gör nyckeln
+    private String readKey(){
+        String[] rKeys = {"U","8","1"};
+        key = rKeys[r.nextInt(rKeys.length)];
+        return key;
     }
+
+    // krypteraren
     public void encrypt(String meddelande, String key){
+
 
     }
 }
