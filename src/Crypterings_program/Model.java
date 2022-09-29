ab37a8a6cd4ad99ea412e1ffe15b7b57144bd082
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2022-09-29 13:43:05 +0200

    har börjat att strukterera modelen

--- src/Crypterings_program/Model.java
+++ src/Crypterings_program/Model.java
@@ -1,4 +1,16 @@
 package Crypterings_program;
 
 public class Model {
+    private char kry_svar;
+
+
+    public void fileReader(){
+
+    }
+
+    public void kryptering (char meddelande, char key){
+        meddelande = 'd';
+        key = 'y';
+        kry_svar = (char) (meddelande^key);
+    }
 }
