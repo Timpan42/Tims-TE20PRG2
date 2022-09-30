ce304c9431fa362957b1fdac3f0d4d0a86464972
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2022-09-30 13:42:01 +0200

    kan göra en char lista med data, kanske kan läsa in fill

--- src/Crypterings_program/Model.java
+++ src/Crypterings_program/Model.java
@@ -1,16 +1,47 @@
 package Crypterings_program;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Model {
     private char kry_svar;
 
+// ska läsa fillen med datan
+    public String fileReader(){
+        String fileInput = "krypt_fill.txt";
+        String data = null;
+        try {
+            File input = new File(fileInput);
+            Scanner reader = new Scanner(input);
+            while (reader.hasNextLine()){
+                data = reader.nextLine();
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Ingen file eller kunde inte läsa file");
+            e.printStackTrace();
+        }
+        return data;
+    }
+    public void kryptering (String data){
+        char[] chars = data.toCharArray();
+        for (int i = 0; i < data.length(); i++) {
+            chars[i] = data.charAt(i);
+        }
+        for (int i = 0; i < chars.length; i++) {
 
-    public void fileReader(){
-
+        }
     }
 
-    public void kryptering (char meddelande, char key){
-        meddelande = 'd';
-        key = 'y';
-        kry_svar = (char) (meddelande^key);
-    }
+    // ska över den krypterade informationen till en fill
+    /* public void fileWriter(){
+        String filOutbut = "outbut.txt";
+        try {
+            FileWriter fw = new FileWriter(filOutbut);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } */
 }
